e8979aa6c5e1e61215a1a6dbf925d121c069442a
root <root@Boost>
2024-01-25 16:52:40 +0700

    first push

---
+++ app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from PIL import Image
+import io
+import numpy as np
+import cv2
+from model import ALPR
+
+# Import your model here
+my_model = ALPR()
+
+st.title('License Plate Recognition')
+
+# Image uploader
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    image = Image.open(io.BytesIO(bytes_data))
+
+    # Convert PIL Image to numpy array
+    image_np = np.array(image.convert('RGB'))
+
+    # Display the uploaded image
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+
+    # Predict or process the input
+    if st.button('Run Model'):
+        # Use the forward method of ALPR class
+        bbox, cropped_img, ocr_result = my_model.forward(image_np)
+
+        if bbox and cropped_img is not None:
+            # Draw bounding box on the original image
+            x1, y1, x2, y2 = bbox
+            cv2.rectangle(image_np, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+
+            # Convert numpy array back to PIL Image for display
+            boxed_image = Image.fromarray(image_np)
+            cropped_image = Image.fromarray(cropped_img)
+
+            # Display the images with bounding box and the cropped image
+            st.image(boxed_image, caption="Image with Detected Region", use_column_width=True)
+            st.image(cropped_image, caption="Cropped Image", use_column_width=True)
+
+            # Display the OCR result
+            text, confidence = ocr_result[0][0]
+            st.write("OCR Result: ", text)
+            st.write("Confidence: ", confidence)
+        else:
+            st.write("No license plate detected.")
---
+++ model.py
@@ -0,0 +1,57 @@
+from paddleocr import PaddleOCR
+from ultralytics import YOLO
+import cv2
+import numpy as np
+
+
+class ALPR():
+    def __init__(self):
+        self.model_detect = YOLO('./models/license_plate_detector.pt')
+        self.model_recog = PaddleOCR(use_angle_cls=False, det=False, lang='en', use_gpu=True, ocr_version='PP-OCRv3', vis_font_path='./simfang.ttf', rec_char_dict_path='./plate_dict.txt',  rec_model_dir='./models/plate_rec/', show_log=False)
+    
+    def should_flip_colors(self, image):
+        # Convert the image to grayscale
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Count the number of black and white pixels
+        black_pixels = np.sum(gray_image == 0)
+        white_pixels = np.sum(gray_image == 255)
+
+        # Decide whether to flip colors based on pixel count
+        return black_pixels > white_pixels
+
+    def flip_colors(self, image):
+        if self.should_flip_colors(image):
+            # Invert the colors using bitwise NOT operation
+            inverted_image = cv2.bitwise_not(image)
+            return inverted_image
+        else:
+            return image
+    
+    def detect(self, image):
+        results = self.model_detect(image, verbose=False)[0]
+        try:
+            x1, y1, x2, y2, score, class_id = results.boxes.data.tolist()[0]
+        except:
+            return None
+
+        return x1, y1, x2, y2
+    
+    def read(self, image):
+        img = self.flip_colors(image)
+        res = self.model_recog.ocr(img, det=False, cls=False)
+        return res
+
+    def forward(self, image):
+        try:
+            x1, y1, x2, y2 = self.detect(image)
+        except:
+            return None, None, None  # No detection
+
+        # Crop the image
+        img_crop = image[int(y1):int(y2), int(x1):int(x2)]
+
+        ocr_result = self.read(img_crop)
+
+        # Return bounding box coordinates, cropped image, and OCR result
+        return (x1, y1, x2, y2), img_crop, ocr_result
